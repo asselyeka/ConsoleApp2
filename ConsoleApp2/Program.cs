758371ac504d6955bc7e338ea3cc9802cc413cec
Asselya Moldasheva <a.moldasheva@gmail.com>
2019-06-24 17:07:58 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите два числа:");
+            string a, b;
+            a = Console.ReadLine();
+            b = Console.ReadLine();
+            int sum = int.Parse(a)+int.Parse(b);
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
